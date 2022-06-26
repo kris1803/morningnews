32c23ffaeb86d4b9e62670798a8ab2b0eea62eaf
kris1803 <78277770+kris1803@users.noreply.github.com>
2022-06-26 16:49:36 +0200

    added 404 page frontend

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 //import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit'
 import './App.css';
+
 import ScreenHome from './pages/Login/ScreenLogin';
 import ScreenMyArticles from './pages/MyArticles/ScreenMyArticles';
 import ScreenSource from './pages/Source/ScreenSource';
 import ScreenArticlesBySource from './pages/ArticlesBySource/ScreenArticlesBySource';
+import ScreenError from './pages/Error/ScreenError';
 
 import user from './reducers/user.reducer.js';
 import wishlist from './reducers/wishlist.reducer.js';
@@ -23,6 +26,7 @@ function App() {
           <Route path="/my-articles" component={ScreenMyArticles} />
           <Route path="/source" component={ScreenSource} />
           <Route path="/articles-by-source/:id" component={ScreenArticlesBySource} />
+          <Route path="*" component={ScreenError} />
         </Switch>
       </Router>
     </Provider>
